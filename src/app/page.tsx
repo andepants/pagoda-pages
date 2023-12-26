e522e6990a65d1373387b9ce5db31dbf9928b676
Andrew Heim <andrewsheim@gmail.com>
2023-12-25 17:30:06 -0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center justify-center min-h-screen">
       <h1 className="texdark-500 text-4xl font-bold flex justify-center">
-        Pagoda Pages
+        Pagoda Pages!
       </h1>
     </main>
   )
